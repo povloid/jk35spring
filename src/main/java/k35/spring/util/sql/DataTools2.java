0637a4a6eeecfbeb39b8e396e95b663114f9ab09
Pavel Kopychenko <t34box@gmail.com>
2024-11-25 22:34:03 +0300

    Work on convertors...

---
+++ src/main/java/k35/spring/util/sql/DataTools2.java
@@ -0,0 +1,79 @@
+package k35.spring.util.sql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.lang.Nullable;
+
+import k35.sql.util.convertors.Transform.TransformFunction;
+
+public final class DataTools2 {
+
+	/**
+	 * Защитим от создания экземляров, будем использовать только статические методы.
+	 */
+	private DataTools2() {
+
+	}
+
+	@FunctionalInterface
+	public static interface PrepeareFunction<T> {
+		SqlParameterSource apply(T o);
+	}
+
+	/**
+	 * Извлечение данных в коллекцию из ResultSet
+	 * 
+	 * @param <T>
+	 * @param transform
+	 * @return
+	 */
+	public static <T> ResultSetExtractor<List<T>> createResultSetExtractorFor(TransformFunction<T> transform) {
+		return rs -> {
+
+			final List<T> list = new ArrayList<>();
+
+			while (rs.next()) {
+				list.add(transform.apply(rs));
+			}
+
+			return list;
+		};
+	}
+
+	/**
+	 * Преобразователь результата по функции
+	 */
+	public static class RowMapperFor<T> implements RowMapper<T> {
+
+		private final TransformFunction<T> transform;
+
+		public RowMapperFor(TransformFunction<T> transform) {
+			this.transform = transform;
+		}
+
+		@Override
+		@Nullable
+		public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return this.transform.apply(rs);
+		}
+
+	}
+
+	/**
+	 * Создать преобразователь записи
+	 * 
+	 * @param <T>
+	 * @param transform
+	 * @return
+	 */
+	public static <T> RowMapper<T> createRowMapperFor(final TransformFunction<T> transform) {
+		return new RowMapperFor<>(transform);
+	}
+
+}
