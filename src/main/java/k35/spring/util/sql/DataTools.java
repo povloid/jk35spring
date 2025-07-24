893200886e17995668e7f2f25f699cd55eda83e6
Pavel Kopychenko <t34box@gmail.com>
2025-07-24 23:19:31 +0300

    Code format.

--- src/main/java/k35/spring/util/sql/DataTools.java
+++ src/main/java/k35/spring/util/sql/DataTools.java
@@ -1,92 +1,91 @@
 package k35.spring.util.sql;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
+import k35.sql.util.convertors.Transform;
+import k35.sql.util.convertors.Transform.ExtractFunction;
+import k35.sql.util.convertors.Transform.TransformFunction;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.lang.Nullable;
 
-import k35.sql.util.convertors.Transform;
-import k35.sql.util.convertors.Transform.ExtractFunction;
-import k35.sql.util.convertors.Transform.TransformFunction;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class DataTools {
 
-	/**
-	 * Защитим от создания экземляров, будем использовать только статические методы.
-	 */
-	private DataTools() {
+    /**
+     * Защитим от создания экземпляров, будем использовать только статические методы.
+     */
+    private DataTools() {
 
-	}
+    }
 
-	@FunctionalInterface
-	public static interface PrepeareFunction<T> {
-		SqlParameterSource apply(T o);
-	}
+    @FunctionalInterface
+    public interface PrepeareFunction<T> {
+        SqlParameterSource apply(T o);
+    }
 
-	/**
-	 * Извлечение данных в коллекцию из ResultSet
-	 * 
-	 * @param <T>
-	 * @param transform
-	 * @return
-	 */
-	public static <T> ResultSetExtractor<List<T>> createResultSetExtractorFor(TransformFunction<T> transform) {
-		return rs -> {
+    /**
+     * Извлечение данных в коллекцию из ResultSet
+     *
+     * @param <T>
+     * @param transform
+     * @return
+     */
+    public static <T> ResultSetExtractor<List<T>> createResultSetExtractorFor(TransformFunction<T> transform) {
+        return rs -> {
 
-			final List<T> list = new ArrayList<>();
+            final List<T> list = new ArrayList<>();
 
-			while (rs.next()) {
-				list.add(transform.apply(rs));
-			}
+            while (rs.next()) {
+                list.add(transform.apply(rs));
+            }
 
-			return list;
-		};
-	}
+            return list;
+        };
+    }
 
-	/**
-	 * Преобразователь результата по функции
-	 */
-	public static class RowMapperFor<T> implements RowMapper<T> {
+    /**
+     * Преобразователь результата по функции
+     */
+    public static class RowMapperFor<T> implements RowMapper<T> {
 
-		private final TransformFunction<T> transform;
+        private final TransformFunction<T> transform;
 
-		public RowMapperFor(TransformFunction<T> transform) {
-			this.transform = transform;
-		}
+        public RowMapperFor(TransformFunction<T> transform) {
+            this.transform = transform;
+        }
 
-		@Override
-		@Nullable
-		public T mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return this.transform.apply(rs);
-		}
+        @Override
+        @Nullable
+        public T mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return this.transform.apply(rs);
+        }
 
-	}
+    }
 
-	/**
-	 * Создать преобразователь записи
-	 * 
-	 * @param <T>
-	 * @param transform
-	 * @return
-	 */
-	public static <T> RowMapper<T> createRowMapperFor(final TransformFunction<T> transform) {
-		return new RowMapperFor<>(transform);
-	}
+    /**
+     * Создать преобразователь записи
+     *
+     * @param <T>
+     * @param transform
+     * @return
+     */
+    public static <T> RowMapper<T> createRowMapperFor(final TransformFunction<T> transform) {
+        return new RowMapperFor<>(transform);
+    }
 
-	/**
-	 * Создать преобразователь записи
-	 * 
-	 * @param <T>
-	 * @param extractFunction
-	 * @return
-	 */
-	public static <T> RowMapper<T> createRowMapperFor(final ExtractFunction<T> extractFunction) {
-		return new RowMapperFor<>(Transform.of(extractFunction));
-	}
+    /**
+     * Создать преобразователь записи
+     *
+     * @param <T>
+     * @param extractFunction
+     * @return
+     */
+    public static <T> RowMapper<T> createRowMapperFor(final ExtractFunction<T> extractFunction) {
+        return new RowMapperFor<>(Transform.of(extractFunction));
+    }
 
 }
